3911fa93f106b65270731a7ccd7e1e487168e596
BU33R <t.laubepin@codeur.online>
2021-10-06 16:48:04 +0200

    Ajout de musique

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,5 @@
 var sound = new Howl({
-    src: ['sound.webm', 'sound.mp3']
-  });
-  
-  // Play returns a unique Sound ID that can be passed
-  // into any method on Howl to control that specific sound.
-  var id1 = sound.play();
-  var id2 = sound.play();
-  
-  // Fade out the first sound and speed up the second.
-  sound.fade(1, 0, 1000, id1);
-  sound.rate(1.5, id2);
-  
+  src: ['sound.mp3']
+});
+
+sound.play();
